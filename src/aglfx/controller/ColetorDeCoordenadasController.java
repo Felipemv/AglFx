055063c3cc0744729066274ee921baef57bf0ea6
Felipemv <felipe.martinsvitor@gmail.com>
2018-01-28 20:07:22 -0200

    Preparado para iniciar coleta de coordenadas.

--- src/aglfx/controller/ColetorDeCoordenadasController.java
+++ src/aglfx/controller/ColetorDeCoordenadasController.java
@@ -5,14 +5,17 @@
  */
 package aglfx.controller;
 
+import aglfx.model.bean.Pallet;
 import com.sun.javafx.geom.Point2D;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -41,12 +44,15 @@ public class ColetorDeCoordenadasController implements Initializable {
 
     private GraphicsContext gc;
     private List<Rectangle> blocos;
+    private List<Pallet> pallets;
     private Point2D inicio;
     private Point2D fim;
-    private Rectangle retangulo;
     private int stackSize;
     private Point2D posClick;
     private Point2D translate;
+    
+    private final int RAIO_ELLIPSE = 5;
+    private final int MARGEM_MARCACAO = 3;
 
     @FXML
     private Pane paneFerramentas;
@@ -73,20 +79,53 @@ public class ColetorDeCoordenadasController implements Initializable {
     private ToggleButton tBtnMover;
 
     @FXML
-    private void stackOnMouseClickedListener(MouseEvent event) {        
-        if(tBtnPonto.isSelected()){
+    private void stackOnMouseClickedListener(MouseEvent event) {
+        if (tBtnPonto.isSelected()) {
             double coordenadaX = event.getX();
             double coordenadaY = event.getY();
 
-            int eSize = 8;
-
-            Ellipse ellipse = new Ellipse(0, 0, eSize, eSize);
-            ellipse.setTranslateX(coordenadaX - eSize);
-            ellipse.setTranslateY(coordenadaY - eSize);
+            Ellipse ellipse = new Ellipse(RAIO_ELLIPSE, RAIO_ELLIPSE);
+            ellipse.setTranslateX(coordenadaX - RAIO_ELLIPSE);
+            ellipse.setTranslateY(coordenadaY - RAIO_ELLIPSE);
             ellipse.setFill(Color.BLUE);
-
+            
             stack.getChildren().add(ellipse);
             stackSize++;
+
+            ellipse.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+                    if (tBtnMover.isSelected()) {
+                        posClick = new Point2D((float) event1.getSceneX(),
+                                (float) event1.getSceneY());
+
+                        translate = new Point2D((float) ellipse.getTranslateX(),
+                                (float) ellipse.getTranslateY());
+                    }
+                }
+            });
+
+            ellipse.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+                    if (tBtnMover.isSelected()) {
+                        ellipse.setCursor(Cursor.MOVE);
+                        double deslocX = event1.getSceneX() - posClick.x;
+                        double deslocY = event1.getSceneY() - posClick.y;
+
+                        ellipse.setTranslateX(deslocX + translate.x);
+                        ellipse.setTranslateY(deslocY + translate.y);
+                    }
+                }
+            });
+
+            ellipse.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+                    ellipse.setCursor(Cursor.DEFAULT);
+                }
+
+            });
         }
     }
 
@@ -96,8 +135,12 @@ public class ColetorDeCoordenadasController implements Initializable {
         if (tBtnBloco.isSelected()) {
             fim = new Point2D((float) event.getX(), (float) event.getY());
 
-            retangulo = new Rectangle(Math.abs(fim.x - inicio.x), Math.abs(fim.y - inicio.y));
-            
+            double width = fim.x - inicio.x;
+            double height = fim.y - inicio.y;
+
+            Rectangle retangulo = new Rectangle(Math.abs(width), Math.abs(height));
+            retangulo.setFill(Color.rgb(0, 0, 0, 0.3));
+
             if (inicio.x > fim.x) {
                 retangulo.setTranslateX(fim.x);
                 if (inicio.y > fim.y) {
@@ -121,27 +164,85 @@ public class ColetorDeCoordenadasController implements Initializable {
             }
 
             stack.getChildren().add(retangulo);
-                        
-            retangulo.setOnMousePressed((MouseEvent event1) -> {
-                if(tBtnMover.isSelected()){
-                    posClick = new Point2D((float)event1.getSceneX(), 
-                            (float)event1.getSceneY());
-                    
-                    translate = new Point2D((float) retangulo.getTranslateX(), 
-                        (float) retangulo.getTranslateY());
+
+            retangulo.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+                    if (tBtnMover.isSelected()) {
+                        retangulo.setCursor(Cursor.MOVE);
+                        posClick = new Point2D((float) event1.getSceneX(),
+                                (float) event1.getSceneY());
+
+                        translate = new Point2D((float) retangulo.getTranslateX(),
+                                (float) retangulo.getTranslateY());
+                        System.out.println("ID: "+retangulo.getId());
+                    }
                 }
+
             });
-            retangulo.setOnMouseDragged((MouseEvent event1) -> {
-                if(tBtnMover.isSelected()){
-                    double deslocX = event1.getSceneX() - posClick.x;
-                    double deslocY = event1.getSceneY() - posClick.y;
-                    
-                    retangulo.setTranslateX(deslocX + translate.x);
-                    retangulo.setTranslateY(deslocY + translate.y);
+
+            retangulo.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+                    if (tBtnMover.isSelected()) {
+                        double deslocX = event1.getSceneX() - posClick.x;
+                        double deslocY = event1.getSceneY() - posClick.y;
+
+                        retangulo.setTranslateX(deslocX + translate.x);
+                        retangulo.setTranslateY(deslocY + translate.y);
+                    }
+                }
+
+            });
+
+            retangulo.setOnMouseReleased(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+                    retangulo.setCursor(Cursor.DEFAULT);
+                }
+
+            });
+
+            retangulo.setOnMouseMoved(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event1) {
+
+                    //canto esquerdo superior
+                    double x = retangulo.getTranslateX();
+                    double y = retangulo.getTranslateY();
+
+                    //Dimensões no retangulo
+                    double w = retangulo.getWidth();
+                    double h = retangulo.getHeight();
+
+                    //local onde o mouse está
+                    double evtX = event1.getX() + x;
+                    double evtY = event1.getY() + y;
+
+                    if (!tBtnMover.isSelected()) {
+                        //Linha de cima
+                        if (evtX >= x && evtX <= x + w &&  evtY <= y + MARGEM_MARCACAO) {
+                            retangulo.setCursor(Cursor.CROSSHAIR);
+                            
+                        // Linha de baixo    
+                        } else if (evtX >= x && evtX <= x + w && evtY >= y + h - MARGEM_MARCACAO) {
+                            retangulo.setCursor(Cursor.CROSSHAIR);
+                            
+                        // Linha da esquerda
+                        } else if (evtX <= x + MARGEM_MARCACAO && evtY >= y && evtY <= evtY + h) {
+                            retangulo.setCursor(Cursor.CROSSHAIR);
+                            
+                        //Linha da direita
+                        } else if (evtX >= x + w - MARGEM_MARCACAO && evtY >= y && evtY <= y + h) {
+                            retangulo.setCursor(Cursor.CROSSHAIR);                            
+                            
+                        } else {
+                            retangulo.setCursor(Cursor.DEFAULT);
+                        }
+                    }
                 }
             });
         }
-
     }
 
     @FXML
@@ -151,19 +252,12 @@ public class ColetorDeCoordenadasController implements Initializable {
 
     @FXML
     private void stackOnMousePressedListener(MouseEvent event) {
-        
         inicio = new Point2D((float) event.getX(), (float) event.getY());
     }
 
     @FXML
     private void stackOnMouseReleasedListener(MouseEvent event) {
         stackSize++;
-        
-    }
-
-    @FXML
-    private void stackOnMouseEnteredListener(MouseEvent event) {
-//        stack.setCursor(Cursor.HAND);
     }
 
     /**
